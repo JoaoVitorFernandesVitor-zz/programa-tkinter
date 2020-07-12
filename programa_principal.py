da885d3732aadd28705f8b27ac6c728f1ee469da
João Vitor Fernandes Vitor <joaovitofernandesvitor@gmail.com>
2020-07-12 18:53:53 -0300

    Organizando o codigo e adicionando mais propriedades na tela

--- programa_principal.py
+++ programa_principal.py
@@ -2,19 +2,24 @@ from tkinter import *
 from fucoes import *
 
 
-#criando a tela principal
+#Iniciando a tela principal
 screen = Tk()
+
+#Propriedades da tela
 altura = 600
 largura = 800
+titulo = 'Login'
+icone = ''
+
 #centralizando a tela
 largura_screen = screen.winfo_screenwidth()
 altura_screen = screen.winfo_screenheight()
 posx = largura_screen/2 - largura/2
 posy = altura_screen/2 - altura/2
 
+#Programação da Tela
 screen.geometry('%dx%d+%d+%d'%(largura, altura, posx, posy))
-
-
-
+screen.title(titulo)
+#screen.icon= icone
 
 screen.mainloop()
