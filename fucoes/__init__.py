3590e805df9eb586babde7c5cab404f6546acc83
João Vitor Fernandes Vitor <joaovitofernandesvitor@gmail.com>
2020-07-12 20:52:00 -0300

    Adicionei funcao do centro e começei a nova classe login
    Transformei o calculo de centralizar a tela pois vou utilizar na classe e parece muito util para o futuro provavelmente usaria ela para outra classe

--- fucoes/__init__.py
+++ fucoes/__init__.py
@@ -1,26 +1,29 @@
 from tkinter import *
+def Centerx(parent, lar):
+    #Fução q acha o centro x da resolução
+    largura_screen = parent.winfo_screenwidth()
+    posx = largura_screen/2 - lar/2
+    return posx
 
+def Centery(parent,alt):
+    #Fução q acha o centro y da resolução
+    altura_screen = parent.winfo_screenheight()
+    posy = altura_screen/2 - alt/2
+    return posy
 
-
-#criando botao
-class Botao(Button):
-    def __init__(self,parent , texto, row_ = 0 , column_ = 1):
+#Criando Frame Login
+class Login(Frame):
+    def __init__(self, parent):
         super().__init__()
-        self['text'] = texto
-        self.grid(row = row_, column= column_ )
+        #propriedades do Frame
+        self['bg'] = 'black'
+        self['heigh'] = 600
+        self['width'] = 800
+        self['relief'] = SOLID
 
-#Criando Entrada de Texto
-class MinhaEntry(Entry):
-    def __init__(self, parent, row_ = 0, column_ = 0):
-        super().__init__()
-        self['width'] = 50
-        self['bd'] = 5
-        self.grid(row = row_, column = column_)
-
-#Criando Label      
-class label(Label):
-    def __init__(self, parent, texto = '', row_ = 0, column_ = 0 , borda = 0):
-        super().__init__()
-        self['text'] = texto
-        self.grid(row = row_, column = column_)
+        #widgets
     
+        #Widgets Grid
+        self.grid()
+    #validação
+    #autodestruir
--- programa_principal.py
+++ programa_principal.py
@@ -12,23 +12,15 @@ largura = 800
 titulo = 'Login'
 icone = ''
 
-#centralizando a tela
-largura_screen = screen.winfo_screenwidth()
-altura_screen = screen.winfo_screenheight()
-posx = largura_screen/2 - largura/2
-posy = altura_screen/2 - altura/2
+#Centralizando a Tela
+tela_x = Centerx(screen, largura)
+tela_y = Centery(screen, altura)
 
 #Programação da Tela
-screen.geometry('%dx%d+%d+%d'%(largura, altura, posx, posy))
+screen.geometry('%dx%d+%d+%d'%(largura, altura, tela_x, tela_y))
 screen.title(titulo)
 #screen.icon= icone
 
 #Adicionando Widgets
-b1 = Botao(screen,'Entrar')
-e1 = MinhaEntry(screen, )
-l2 = label(screen, 'Senha')
-
-l2['text'] = 'fon'
-
-
+login = Login(screen)
 screen.mainloop()
